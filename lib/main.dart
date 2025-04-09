01d585a993a618c35a99b0024b4182356a0bb881
milan-1936 <khpo_bca2077@lict.edu.np>
2025-04-09 11:59:55 +0545

    Added button and event handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_2/assignment.dart';
-import 'package:flutter_2/container_container.dart';
-import 'package:flutter_2/day2.dart';
-import 'package:flutter_2/homepage.dart';
-import 'package:flutter_2/images.dart';
+import 'package:flutter_2/counter_app.dart';
+// import 'package:flutter_2/assignment.dart';
+// import 'package:flutter_2/container_container.dart';
+// import 'package:flutter_2/day2.dart';
+// import 'package:flutter_2/homepage.dart';
+// import 'package:flutter_2/images.dart';monitor
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       debugShowCheckedModeBanner: false,
-      home: Imagess(),
+      home: CounterApp(),
     );
   }
 }
