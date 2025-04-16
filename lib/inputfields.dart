53a90593c13e8a2e22037fc0b013e0ba5447114a
milan-1936 <khpo_bca2077@lict.edu.np>
2025-04-16 11:53:27 +0545

    Forms and toggle button, checkboxes

--- lib/inputfields.dart
+++ lib/inputfields.dart
@@ -3,13 +3,16 @@ import 'package:flutter/material.dart';
 class formWidget extends StatefulWidget {
    formWidget({super.key});
 
-  final GlobalKey<FormState> _nameFormKey = GlobalKey();
+  
 
   @override
   State<formWidget> createState() => _formWidgetState();
 }
 
 class _formWidgetState extends State<formWidget> {
+  final GlobalKey<FormState> _nameFormKey = GlobalKey();
+  bool switchValue = true;
+  bool checkedValue = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,13 +20,24 @@ class _formWidgetState extends State<formWidget> {
         title: Text("Form Example"),
       ),
       body: Form(
-        key: GlobalKey(),
+        key: _nameFormKey,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
               TextFormField(
+                validator: (value) {
+                  if(value == null || value.isEmpty){
+                    return "Username Haan na vaneko";
+                  }else{
+                    return null;
+                  }
+                },
+                onSaved: (value){
+                    print(value!);
+                },
                 decoration:InputDecoration(
+                  prefixIcon: Icon(Icons.person),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
@@ -31,16 +45,41 @@ class _formWidgetState extends State<formWidget> {
                   labelText: "Name"
                 ),
               ),
+
               FilledButton(
                 onPressed: (){
-
+                  if(!_nameFormKey.currentState!.validate()){
+                    return;
+                  }
+                  _nameFormKey.currentState!.save();
                 },
                 child: Text("Submit"),
               ),
+
+              Switch(
+                value: switchValue, 
+                onChanged: (value){
+                  setState((){
+                    switchValue = !switchValue;
+                  });
+              },
+              ),
+
+              Checkbox(
+                value: checkedValue, 
+                onChanged: (value) {
+                  setState((){
+                    checkedValue = value ?? false;
+                  });
+              }
+              ),
+
+
+            
             ],
             ),
         ),
-        
+
           )
     );
   }
